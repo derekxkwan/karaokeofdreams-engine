66c7bd1972f911e1c590ff16e0b8fa1dbd0201ef
Derek Kwan <derek.x.kwan@gmail.com>
2020-03-10 23:17:01 -0700

    update script

--- util/lyrics_texts.rb
+++ util/lyrics_texts.rb
@@ -1,2 +1,10 @@
-print Dir.each_child(File.dirname(__FILE__) + "/../res").to_a.map{|x| File.basename(x, ".txt")}
+curdir = File.dirname(__FILE__)
+lyricfiles = Dir.each_child(curdir + "/../res").to_a
+basefiles = lyricfiles.map{|x| File.basename(x, ".txt")}
+print basefiles
 puts ""
+basestr = basefiles.map{|x| x.downcase.tr(" ", "_")}.join("\n")
+# basestr = basefiles.join("\n")
+File.open(curdir + "/../pd/midifiles.txt", "w") do |cur|  
+  cur.write(basestr)
+end
